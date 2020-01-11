c35605be921e802d6bacce52a3545bc490c2c000
haibarawu <ywu78@binghamton.edu>
2020-01-10 18:11:06 -0800

    Create 0577.  Employee Bonus.sql

---
+++ Database/0577.  Employee Bonus.sql
@@ -0,0 +1,44 @@
+/********************************************************************************
+577.  Employee Bonus
+
+Difficulty: Easy
+
+Table:Employee
++-------+--------+-----------+--------+
+| empId |  name  | supervisor| salary |
++-------+--------+-----------+--------+
+|   1   | John   |  3        | 1000   |
+|   2   | Dan    |  3        | 2000   |
+|   3   | Brad   |  null     | 4000   |
+|   4   | Thomas |  3        | 4000   |
++-------+--------+-----------+--------+
+empId is the primary key column for this table.
+
+Table: Bonus
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+empId is the primary key column for this table.
+
+Select all employee's name and bonus whose bonus is < 1000.
+
+Example ouput:
++-------+-------+
+| name  | bonus |
++-------+-------+
+| John  | null  |
+| Dan   | 500   |
+| Brad  | null  |
++-------+-------+
+********************************************************************************/
+
+
+SELECT E.name, B.bonus
+FROM Employee AS E
+LEFT JION Bonus AS B
+ON E.empid = B.empid
+WHERE B.bonus < 1000 OR B.bonus IS NULL
+
