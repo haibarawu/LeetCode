b5f2ecc83af71470f211b09538999a9a92a7f931
haibarawu <ywu78@binghamton.edu>
2020-01-13 14:05:46 -0800

    Create 1069.(Easy) Product Sales Analysis II.sql

---
+++ Database/1069.(Easy) Product Sales Analysis II.sql
@@ -0,0 +1,40 @@
+/********************************************************************************
+1069. Product Sales Analysis II
+
+Difficulty: Easy
+
+Write an SQL query that reports the total quantity sold for every product id.
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++--------------+----------------+
+| product_id   | total_quantity |
++--------------+----------------+
+| 100          | 22             |
+| 200          | 15             |
++--------------+----------------+
+********************************************************************************/
+
+
+SELECT product_id, SUM(quantity) AS total_quantity
+FROM Sales
+GROUP BY product_id
+
