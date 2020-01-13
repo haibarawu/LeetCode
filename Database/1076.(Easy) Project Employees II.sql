99e6ab427cf3dd41fe32ee3a5058302e482a51b8
haibarawu <ywu78@binghamton.edu>
2020-01-13 14:52:29 -0800

    Create 1076.(Easy) Project Employees II.sql

---
+++ Database/1076.(Easy) Project Employees II.sql
@@ -0,0 +1,72 @@
+/********************************************************************************
+1076.Project Employees II
+
+Difficulty: Easy
+
+Table: Project
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+
+Table: Employee
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+
+Write an SQL query that reports all the projects that have the most employees.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+
+| project_id  |
++-------------+
+| 1           |
++-------------+
+The first project has 3 employees while the second one has 2.
+
+********************************************************************************/
+
+
+SELECT project_id
+FROM Project
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 
+(
+  SELECT TOP 1 COUNT(employee_id) AS emp_count
+  FROM Project
+  GROUP BY employee_id
+  ORDER BY emp_count DESC
+)
+
