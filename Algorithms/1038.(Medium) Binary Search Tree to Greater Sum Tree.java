a8c6aa9a59aa08c2e952fec849cf1f93aa0458c5
haibarawu <ywu78@binghamton.edu>
2020-01-20 12:24:25 -0800

    Update 1038.(Medium) Binary Search Tree to Greater Sum Tree.java

--- Algorithms/1038.(Medium) Binary Search Tree to Greater Sum Tree.java
+++ Algorithms/1038.(Medium) Binary Search Tree to Greater Sum Tree.java
@@ -3,6 +3,9 @@
 
 Difficulty: Medium
 
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/
+
 Given the root of a binary search tree with distinct values, 
 modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.
 val.
