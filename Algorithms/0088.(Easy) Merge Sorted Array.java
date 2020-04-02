4db0bbee7a2fc276e93837979242a8a2269f32ab
haibarawu <ywu78@binghamton.edu>
2020-04-01 21:10:25 -0700

    Update and rename 088. Merge Sorted Array to Algorithms/0088.(Easy) Merge Sorted Array.java

--- Algorithms/0088.(Easy) Merge Sorted Array.java
+++ Algorithms/0088.(Easy) Merge Sorted Array.java
@@ -1,9 +1,20 @@
-/*
+/****************************************************************************************************
+88. Merge Sorted Array
+
+Difficulty: Easy
+
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
 Note:
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. 
 The number of elements initialized in nums1 and nums2 are m and n respectively.
-*/
+
+Example:
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+Output: [1,2,2,3,5,6]
+****************************************************************************************************/
 
 //Java solution:
 
@@ -25,13 +36,13 @@ public class Solution {
                j--;
             }
         }
-        
+        /* 不需要处理第1个数组里面的数字，剩下的即为最小的数字们，留在当下位置即是。
         while(i >= 0){
             nums1[index] = nums1[i];
                 index--;
                 i--;
         }
-        
+        */
         while(j >= 0){
             nums1[index] = nums2[j];
                 index--;
