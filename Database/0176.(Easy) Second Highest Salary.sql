d263375ddc93c6c1ef16ed6bb874ebfc44ec2c04
haibarawu <ywu78@binghamton.edu>
2020-06-02 13:37:56 -0700

    Update and rename 0176. Second Highest Salary.sql to 0176.(Easy) Second Highest Salary.sql

--- Database/0176.(Easy) Second Highest Salary.sql
+++ Database/0176.(Easy) Second Highest Salary.sql
@@ -1,9 +1,11 @@
-/*
-0176. Second Highest Salary
+/****************************************************************************************************
+176. Second Highest Salary
+176. 第二高的薪水
 
 Difficulty: Easy
 
 Write a SQL query to get the second highest salary from the Employee table.
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
 +----+--------+
 | Id | Salary |
 +----+--------+
@@ -12,15 +14,24 @@ Write a SQL query to get the second highest salary from the Employee table.
 | 3  | 300    |
 +----+--------+
 
+SQL 架构：
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+
 For example, given the above Employee table, the query should return 200 as the second highest salary. 
 If there is no second highest salary, then the query should return null.
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
 +---------------------+
 | SecondHighestSalary |
 +---------------------+
 | 200                 |
 +---------------------+
 
-*/
+****************************************************************************************************/
+
 
 --Method1:
 SELECT MAX(Salary) AS SecondHighestSalary 
@@ -32,7 +43,7 @@ WHERE Salary < (SELECT MAX(Salary) FROM Employee)
 WITH salaryRank AS
 (
     SELECT *, 
-            RANK() OVER(ORDER BY Salary Desc) AS rank
+            DENSE_RANK() OVER(ORDER BY Salary DESC) AS rank
     FROM Employee
 )
 
@@ -41,4 +52,5 @@ SELECT ISNULL(
      FROM salaryRank
      WHERE rank = 2)
         , NULL) AS SecondHighestSalary
+
 
