ede84aa24cc6210efb9fc33b6b9c09d3ad9c727f
haibarawu <ywu78@binghamton.edu>
2020-06-02 16:35:40 -0700

    Update 0177.(Medium) Nth Highest Salary.sql

--- Database/0177.(Medium) Nth Highest Salary.sql
+++ Database/0177.(Medium) Nth Highest Salary.sql
@@ -1,9 +1,11 @@
 /****************************************************************************************************
-0177. Nth Highest Salary
+177. Nth Highest Salary
+177. 第 N 高的薪水
 
 Difficulty: Medium
 
 Write a SQL query to get the nth highest salary from the Employee table.
+编写一个 SQL 查询，获取 Employee 表中第 n 高的薪水（Salary）。
 +----+--------+
 | Id | Salary |
 +----+--------+
@@ -12,8 +14,16 @@ Write a SQL query to get the nth highest salary from the Employee table.
 | 3  | 300    |
 +----+--------+
 
+SQL 架构：
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+
 For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
 If there is no nth highest salary, then the query should return null.
+例如上述 Employee 表，n = 2 时，应返回第二高的薪水 200。如果不存在第 n 高的薪水，那么查询应返回 null。
 +------------------------+
 | getNthHighestSalary(2) |
 +------------------------+
@@ -21,6 +31,7 @@ If there is no nth highest salary, then the query should return null.
 +------------------------+
 
 ****************************************************************************************************/
+
 
 -- Method1:
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
@@ -30,7 +41,7 @@ BEGIN
         SELECT ISNULL(
             (SELECT DISTINCT Salary
              FROM (SELECT Salary, 
-                   DENSE_RANK() OVER(ORDER BY Salary DESC) AS rank
+                          DENSE_RANK() OVER(ORDER BY Salary DESC) AS rank
                    FROM Employee) AS r
              WHERE r.rank = @N)
                 , NULL) AS NthHighestSalary
