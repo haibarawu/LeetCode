03c052c967c499cf4865af948a548da205c87cad
haibarawu <ywu78@binghamton.edu>
2020-06-04 13:47:48 -0700

    Update 0197.(Easy) Rising Temperature.sql

--- Database/0197.(Easy) Rising Temperature.sql
+++ Database/0197.(Easy) Rising Temperature.sql
@@ -1,10 +1,20 @@
 /****************************************************************************************************
 197. Rising Temperature
+197. 上升的温度
 
 Difficulty: Easy
 
+SQL Schema:
+Create table If Not Exists Weather (Id int, RecordDate date, Temperature int)
+Truncate table Weather
+insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10')
+insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25')
+insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20')
+insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30')
+
 Given a Weather table, 
 write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+给定一个 Weather 表，编写一个 SQL 查询，来查找与之前（昨天的）日期相比温度更高的所有日期的 Id。
 +---------+------------------+------------------+
 | Id(INT) | RecordDate(DATE) | Temperature(INT) |
 +---------+------------------+------------------+
@@ -15,6 +25,7 @@ write a SQL query to find all dates' Ids with higher temperature compared to its
 +---------+------------------+------------------+
 
 For example, return the following Ids for the above Weather table:
+例如，根据上述给定的 Weather 表格，返回如下 Id:
 +----+
 | Id |
 +----+
@@ -33,6 +44,13 @@ AND DATEDIFF(day, w1.RecordDate, w2.RecordDate) = 1
 
 
 --Method2:
+SELECT w2.Id
+FROM Weather AS w1, Weather AS w2
+WHERE w1.Temperature < w2.Temperature
+AND DATEDIFF(day, w1.RecordDate, w2.RecordDate) = 1
+
+
+--Method3:
 /****************************************************************************************************
 --This won't work, if the date is not consist. For example: 
 +---------+------------------+------------------+
