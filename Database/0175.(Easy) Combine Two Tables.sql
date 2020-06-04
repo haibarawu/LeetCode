cf6f2fa29db2b19bdd8905210292e1faa9783637
haibarawu <ywu78@binghamton.edu>
2020-06-04 16:46:57 -0700

    Update and rename 0175. Combine Two Tables.sql to 0175.(Easy) Combine Two Tables.sql

--- Database/0175.(Easy) Combine Two Tables.sql
+++ Database/0175.(Easy) Combine Two Tables.sql
@@ -1,7 +1,15 @@
 /****************************************************************************************************
-0175. Combine Two Tables
+175. Combine Two Tables
+175. 组合两个表
 
 Difficulty: Easy
+
+Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255))
+Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255))
+Truncate table Person
+Insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
+Truncate table Address
+Insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
 
 Table: Person
 +-------------+---------+
@@ -30,7 +38,6 @@ FirstName, LastName, City, State
 
 编写一个 SQL 查询，满足条件：无论 person 是否有地址信息，都需要基于上述两表提供 person 的以下信息：
 FirstName, LastName, City, State
-
 ****************************************************************************************************/
 
 
