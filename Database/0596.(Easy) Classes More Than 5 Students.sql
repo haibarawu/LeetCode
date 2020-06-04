d25fe180c75c454e1fd491aab735dfc720a0d445
haibarawu <ywu78@binghamton.edu>
2020-06-04 15:13:21 -0700

    Update and rename 0596. Classes More Than 5 Students.sql to 0596.(Easy) Classes More Than 5 Students.sql

--- Database/0596.(Easy) Classes More Than 5 Students.sql
+++ Database/0596.(Easy) Classes More Than 5 Students.sql
@@ -1,10 +1,26 @@
 /********************************************************************************
 596. Classes More Than 5 Students
+596. 超过5名学生的课
 
 Difficulty: Easy
 
+SQL Schema: 
+Create table If Not Exists courses (student varchar(255), class varchar(255))
+Truncate table courses
+insert into courses (student, class) values ('A', 'Math')
+insert into courses (student, class) values ('B', 'English')
+insert into courses (student, class) values ('C', 'Math')
+insert into courses (student, class) values ('D', 'Biology')
+insert into courses (student, class) values ('E', 'Math')
+insert into courses (student, class) values ('F', 'Computer')
+insert into courses (student, class) values ('G', 'Math')
+insert into courses (student, class) values ('H', 'Math')
+insert into courses (student, class) values ('I', 'Math')
+
 There is a table courses with columns: student and class
 Please list out all classes which have more than or equal to 5 students.
+有一个courses 表 ，有: student (学生) 和 class (课程)。
+请列出所有超过或等于5名学生的课。
 
 For example, the table:
 +---------+------------+
@@ -30,6 +46,8 @@ Should output:
 
 Note:
 The students should not be counted duplicate in each course.
+Note:
+学生在每个课中不应被重复计算。
 ********************************************************************************/
 
 
@@ -37,4 +55,5 @@ SELECT class
 FROM courses
 GROUP BY class
 HAVING COUNT(DISTINCT student) >= 5 
+
 
