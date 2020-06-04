5f0a81478cbb2ad4bd1149734b245ea2bba8d293
haibarawu <ywu78@binghamton.edu>
2020-06-04 12:47:42 -0700

    Update and rename 0182. Duplicate Emails.sql to 0182.(Easy) Duplicate Emails.sql

--- Database/0182.(Easy) Duplicate Emails.sql
+++ Database/0182.(Easy) Duplicate Emails.sql
@@ -1,9 +1,11 @@
 /********************************************************************************
-0182. Duplicate Emails
+182. Duplicate Emails
+182. 查找重复的电子邮箱
 
 Difficulty: Easy
 
 Write a SQL query to find all duplicate emails in a table named Person.
+编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
 +----+---------+
 | Id | Email   |
 +----+---------+
@@ -12,7 +14,15 @@ Write a SQL query to find all duplicate emails in a table named Person.
 | 3  | a@b.com |
 +----+---------+
 
+SQL Schema:
+Create table If Not Exists Person (Id int, Email varchar(255))
+Truncate table Person
+insert into Person (Id, Email) values ('1', 'a@b.com')
+insert into Person (Id, Email) values ('2', 'c@d.com')
+insert into Person (Id, Email) values ('3', 'a@b.com')
+
 For example, your query should return the following for the above table:
+根据以上输入，你的查询应返回以下结果：
 +---------+
 | Email   |
 +---------+
@@ -20,6 +30,7 @@ For example, your query should return the following for the above table:
 +---------+
 
 Note: All emails are in lowercase.
+说明：所有电子邮箱都是小写字母。
 ********************************************************************************/
 
 
@@ -39,4 +50,5 @@ SELECT Email
 FROM Person
 GROUP BY Email
 HAVING COUNT(Email) > 1
+
 
