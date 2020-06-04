272d014b5d7edd55d345dbec82af0e64350ce2d7
haibarawu <ywu78@binghamton.edu>
2020-06-04 16:20:51 -0700

    Update and rename 0620. Not Boring Movies.sql to 0620.(Easy) Not Boring Movies.sql

--- Database/0620.(Easy) Not Boring Movies.sql
+++ Database/0620.(Easy) Not Boring Movies.sql
@@ -1,13 +1,26 @@
 /********************************************************************************
 620. Not Boring Movies
+620. 有趣的电影
 
 Difficulty: Easy
 
 https://leetcode.com/articles/not-boring-movies/
 
+SQL Schema:
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1))
+Truncate table cinema
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9')
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5')
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2')
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6')
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1')
+
 X city opened a new cinema, many people would like to go to this cinema. 
 The cinema also gives out a poster indicating the movies’ ratings and descriptions.
 Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
+某城市开了一家新的电影院，吸引了很多人过来看电影。
+该电影院特别注意用户体验，专门有个 LED显示板做电影推荐，上面公布着影评和相关电影描述。
+作为该电影院的信息部主管，您需要编写一个 SQL查询，找出所有影片描述为非 boring (不无聊) 的并且 id 为奇数 的影片，结果请按等级 rating 排列。
 
 For example, table cinema:
 +---------+-----------+--------------+-----------+
@@ -35,4 +48,5 @@ FROM cinema
 WHERE (id % 2) != 0
     AND description != 'boring'
 ORDER BY rating DESC
+
 
