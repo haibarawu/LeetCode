a1e5e0c139eaafd2d0fcba8e27b3b265f9bcd6d4
haibarawu <ywu78@binghamton.edu>
2020-06-04 13:07:11 -0700

    Update 0184.(Medium) Department Highest Salary.sql

--- Database/0184.(Medium) Department Highest Salary.sql
+++ Database/0184.(Medium) Department Highest Salary.sql
@@ -1,9 +1,24 @@
-/********************************************************************************
+/****************************************************************************************************
 184. Department Highest Salary
+184. 部门工资最高的员工
 
 Difficulty: Medium
 
+SQL Schema:
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int)
+Create table If Not Exists Department (Id int, Name varchar(255))
+Truncate table Employee
+insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Jim', '90000', '1')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Henry', '80000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Sam', '60000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('5', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (Id, Name) values ('1', 'IT')
+insert into Department (Id, Name) values ('2', 'Sales')
+
 The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+Employee 表包含所有员工信息，每个员工有其对应的 Id, salary 和 department Id。
 +----+-------+--------+--------------+
 | Id | Name  | Salary | DepartmentId |
 +----+-------+--------+--------------+
@@ -15,6 +30,7 @@ The Employee table holds all employees. Every employee has an Id, a salary, and 
 +----+-------+--------+--------------+
 
 The Department table holds all departments of the company.
+Department 表包含公司所有部门的信息。
 +----+----------+
 | Id | Name     |
 +----+----------+
@@ -24,6 +40,7 @@ The Department table holds all departments of the company.
 
 Write a SQL query to find employees who have the highest salary in each of the departments. 
 For the above tables, your SQL query should return the following rows (order of rows does not matter).
+编写一个 SQL 查询，找出每个部门工资最高的员工。
 +------------+----------+--------+
 | Department | Employee | Salary |
 +------------+----------+--------+
@@ -34,7 +51,8 @@ For the above tables, your SQL query should return the following rows (order of 
 
 Explanation:
 Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
-********************************************************************************/
+例如，根据上述给定的表格，Max 和 Jim 在 IT 部门有最高工资，Henry 在 Sales 部门有最高工资。
+****************************************************************************************************/
 
 
 --Method1: 
@@ -73,3 +91,4 @@ WHERE
         GROUP BY DepartmentId
 	)
   
+
