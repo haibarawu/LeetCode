9c68a476fd944721749cb93194c3d7cbf221ec40
haibarawu <ywu78@binghamton.edu>
2020-06-04 16:04:46 -0700

    Update 0601.(Hard) Human Traffic of Stadium.sql

--- Database/0601.(Hard) Human Traffic of Stadium.sql
+++ Database/0601.(Hard) Human Traffic of Stadium.sql
@@ -1,11 +1,25 @@
 /****************************************************************************************************
 601. Human Traffic of Stadium
+601. 体育馆的人流量
 
 Difficulty: Hard
 
+Create table If Not Exists stadium (id int, visit_date DATE NULL, people int)
+Truncate table stadium
+insert into stadium (id, visit_date, people) values ('1', '2017-01-01', '10')
+insert into stadium (id, visit_date, people) values ('2', '2017-01-02', '109')
+insert into stadium (id, visit_date, people) values ('3', '2017-01-03', '150')
+insert into stadium (id, visit_date, people) values ('4', '2017-01-04', '99')
+insert into stadium (id, visit_date, people) values ('5', '2017-01-05', '145')
+insert into stadium (id, visit_date, people) values ('6', '2017-01-06', '1455')
+insert into stadium (id, visit_date, people) values ('7', '2017-01-07', '199')
+insert into stadium (id, visit_date, people) values ('8', '2017-01-08', '188')
+
 X city built a new stadium, each day many people visit it and the stats are saved as these columns: id, visit_date, people
+X 市建了一个新的体育馆，每日人流量信息被记录在这三列信息中：序号 (id)、日期 (visit_date)、 人流量 (people)。
 
 Please write a query to display the records which have 3 or more consecutive rows and the amount of people more than 100(inclusive).
+请编写一个查询语句，找出人流量的高峰期。高峰期时，至少连续三行记录中的人流量不少于100。
 
 For example, the table stadium:
 +------+------------+-----------+
@@ -33,7 +47,8 @@ For the sample data above, the output is:
 
 Note:
 Each day only have one row record, and the dates are increasing with id increasing.
-
+提示：
+每天只有一行记录，日期随着 id 的增加而增加。
 ****************************************************************************************************/
 
 
