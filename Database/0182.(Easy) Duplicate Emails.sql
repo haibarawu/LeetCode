7b119da749d33d2769965b7d02068beb4649c430
haibarawu <ywu78@binghamton.edu>
2020-06-04 13:19:30 -0700

    Update 0182.(Easy) Duplicate Emails.sql

--- Database/0182.(Easy) Duplicate Emails.sql
+++ Database/0182.(Easy) Duplicate Emails.sql
@@ -4,6 +4,13 @@
 
 Difficulty: Easy
 
+SQL Schema:
+Create table If Not Exists Person (Id int, Email varchar(255))
+Truncate table Person
+insert into Person (Id, Email) values ('1', 'a@b.com')
+insert into Person (Id, Email) values ('2', 'c@d.com')
+insert into Person (Id, Email) values ('3', 'a@b.com')
+
 Write a SQL query to find all duplicate emails in a table named Person.
 编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
 +----+---------+
