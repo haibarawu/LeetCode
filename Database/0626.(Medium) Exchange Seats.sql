6876c6b2114e18911f3d24b553debf9064a8903f
haibarawu <ywu78@binghamton.edu>
2020-06-04 16:12:25 -0700

    Update 0626.(Medium) Exchange Seats.sql

--- Database/0626.(Medium) Exchange Seats.sql
+++ Database/0626.(Medium) Exchange Seats.sql
@@ -1,5 +1,6 @@
 /****************************************************************************************************
 626. Exchange Seats
+626. 换座位
 
 Difficulty: Medium
 
@@ -9,6 +10,10 @@ Mary is a teacher in a middle school and she has a table seat storing students' 
 The column id is continuous increment.
 Mary wants to change seats for the adjacent students.
 Can you write a SQL query to output the result for Mary?
+小美是一所中学的信息科技老师，她有一张 seat 座位表，平时用来储存学生名字和与他们相对应的座位 id。
+其中纵列的 id 是连续递增的
+小美想改变相邻俩学生的座位。
+你能不能帮她写一个 SQL query 来输出小美想要的结果呢？
 
 +---------+---------+
 |    id   | student |
@@ -34,6 +39,8 @@ For the sample input, the output is:
 
 Note:
 If the number of students is odd, there is no need to change the last one's seat.
+注意：
+如果学生人数是奇数，则不需要改变最后一个同学的座位。
 ****************************************************************************************************/
 
 
@@ -43,7 +50,22 @@ If the number of students is odd, there is no need to change the last one's seat
 2）判断 id 是否为最后一位，如果是奇数，且为最后一位，则 id + 1。
 ****************************************************************************************************/
 
---Method1: 
+--Method1:
+SELECT
+    (CASE
+        WHEN (id % 2 = 0) THEN (id - 1)
+        WHEN (id % 2 = 1) AND id != (SELECT COUNT(id) FROM seat) THEN (id + 1)
+        ELSE id
+    END) AS id,
+    student
+FROM seat
+ORDER BY id
+
+
+/****************************************************************************************************/
+
+
+--Method2: 
 DECLARE @count int 
 
 SELECT @count = COUNT(id) 
@@ -63,7 +85,7 @@ ORDER BY id
 /****************************************************************************************************/
 
 
---Method2: 
+--Method3: 
 SELECT IIF((id % 2 = 0), id - 1, IIF((id = (SELECT COUNT(id) FROM seat)), id, id + 1)) AS id, student
 FROM seat
 ORDER BY id
