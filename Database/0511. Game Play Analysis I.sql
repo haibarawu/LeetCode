f840e19244ae15bfceaff1e64018e46903472170
haibarawu <ywu78@binghamton.edu>
2020-06-09 12:00:17 -0700

    Update 0511. Game Play Analysis I.sql

--- Database/0511. Game Play Analysis I.sql
+++ Database/0511. Game Play Analysis I.sql
@@ -1,5 +1,6 @@
-/********************************************************************************
+/****************************************************************************************************
 511. Game Play Analysis I
+511. 游戏玩法分析 I
 
 Difficulty: Easy
 
@@ -15,7 +16,12 @@ Table: Activity
 (player_id, event_date) is the primary key of this table.
 This table shows the activity of players of some game.
 Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
+表的主键是 (player_id, event_date)。
+这张表展示了一些游戏玩家在游戏平台上的行为活动。
+每行数据记录了一名玩家在退出平台之前，当天使用同一台设备登录平台后打开的游戏的数目（可能是 0 个）。
+
 Write an SQL query that reports the first login date for each player.
+获取每位玩家 第一次登陆平台的日期。
 
 The query result format is in the following example:
 Activity table:
@@ -37,10 +43,17 @@ Result table:
 | 2         | 2017-06-25  |
 | 3         | 2016-03-02  |
 +-----------+-------------+
-********************************************************************************/
+****************************************************************************************************/
+
+
+/****************************************************************************************************
+解题思路：
+根据用户ID player_id 进行 GROUP BY 的分组，取每组中最小的时间 event_date，即为该用户第一次登录的时间。
+****************************************************************************************************/
 
 
 SELECT player_id, MIN(event_date) AS first_login
 FROM Activity
 GROUP BY player_id
+
 
